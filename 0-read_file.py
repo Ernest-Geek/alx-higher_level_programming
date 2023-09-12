0bdd5fcfb6dba97e60f97326f89c7a1a4cf6010e
Ernest-Geek <ernestampene1@gmail.com>
2023-09-12 15:32:36 -0700

    Refactor read_file function for improved clarity and adhere to PEP 8 naming conventions

---
+++ 0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defining a text file-reading function"""
+
+def read_file(filename=""):
+    """Print the content of a UTF-8 to the standard output"""
+    with open(filename, encoding = "UTF = 8") as f:
+    print(f.read(), end = "")
