571aa61d06fa6cf43e73aea4e994e263c873c204
Ernest-Geek <ernestampene1@gmail.com>
2023-11-13 04:12:14 -0800

    Add script to print 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const firstLine = ('C is fun\n');
+const secondLine = ('Python is cool\n');
+const thirdLine = ('Javascript is amazing');
+console.log(firstLine + secondLine + thirdLine);
