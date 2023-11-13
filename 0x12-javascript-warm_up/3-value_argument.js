963cabd1181e94d2580f636a915197cd272154a4
Ernest-Geek <ernestampene1@gmail.com>
2023-11-13 05:34:51 -0800

    Add script to print the first argument

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+let count = 0;
+for (const arg of args)
+{
+	count++;
+}
+if (count === 0)
+{
+	console.log('No argument');
+}
+else if (count === 1)
+{
+	console.log( args[0]);
+}
+else
+{
+	console.log('Argument found');
+}
