3b9774db887fe05858bd67d4b2e7238bb8c0ab46
Ernest-Geek <ernestampene1@gmail.com>
2023-11-13 05:57:15 -0800

    semistandard corrections

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,19 +1,13 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 let count = 0;
-for (const arg of args)
-{
-	count++;
+for (; count < args.length; count++) {
+// Looping without declaring any variable
 }
-if (count === 0)
-{
-	console.log('No argument');
-}
-else if (count === 1)
-{
-	console.log( args[0]);
-}
-else
-{
-	console.log('Argument found');
+if (count === 0) {
+  console.log('No argument');
+} else if (count === 1) {
+  console.log(args[0]);
+} else {
+  console.log('Argument found');
 }
