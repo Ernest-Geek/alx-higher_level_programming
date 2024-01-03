89f51717f67d32cf327b5e12638a253ea91f65f5
Ernest-Geek <ernestampene1@gmail.com>
2024-01-03 21:15:08 +0000

    Quick Update

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-//fs: Provides method for working with file systems
+// fs: Provides method for working with file systems
 const fs = require('fs');
 
+// Read the file path argument
+const filePath = process.argv[2];
 
-//Read the file path argument
-const file_path = process.argv[2];
-
-
-//Read the path using utf8 encoding
-fs.readFile(file_path, 'utf8', (error, data) => console.log(error || data));
+// Read the path using utf8 encoding
+fs.readFile(filePath, 'utf8', (error, data) => console.log(error || data));
