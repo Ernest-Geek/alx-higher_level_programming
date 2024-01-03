9d7a184824b33e7e06033e5912fa91730269f75a
Ernest-Geek <ernestampene1@gmail.com>
2024-01-03 21:44:16 +0000

    Write content to file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-
-//Get file path and string to write from command line
+// Get file path and string to write from command line
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-
-//Write string to file in utf8 encoding
+// Write string to file in utf8 encoding
 fs.writeFile(filePath, content, 'utf8', error => {
-	if (error) console.log(error);
+  if (error) console.log(error);
 });
