bcacf63a0df1949388a9da32365a2ce7b70cb968
Ernest-Geek <ernestampene1@gmail.com>
2024-01-04 00:05:35 +0000

    Display the status code of a GET request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
 
-
-//Get the URL from the command line argument
+// Get the URL from the command line argument
 const url = process.argv[2];
 
-
-//Make a GET request to the specified URL
+// Make a GET request to the specified URL
 request.get(url).on('response', function (response) {
-	//Display the status code
-	console.log(`code: ${response.statusCode}`);
+  // Display the status code
+  console.log(`code: ${response.statusCode}`);
 });
