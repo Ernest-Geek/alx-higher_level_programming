664113eb6dfc57134f8c15d9c758acbd1a1977aa
Ernest-Geek <ernestampene1@gmail.com>
2024-03-02 19:24:12 +0000

    Add function to square each element of a matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for i in matrix:
+        row = list(map(lambda x: x * x, i))
+        new_matrix.append(row)
+
+    return new_matrix
+
